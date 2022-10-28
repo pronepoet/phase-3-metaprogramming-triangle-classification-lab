6a7450c5a588b68c33109c4c33789553e76f50cf
Pronepoet <ochiengphillip84@gmail.com>
2022-10-29 01:06:35 +0300

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
   # write code here
-end
+  def initialize(a,b,c)
+  @a=a
+  @b=b
+  @c=c
+  end
+
+
+  def kind
+    if !@a.positive? || !@b.positive? || !@c.positive?
+      raise TriangleError
+    elsif @a +@b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+    end
+
+    if @a == @b
+      if @b == @c
+        :equilateral
+      else
+      :isosceles
+      end
+      elsif @b == @c || @c == @a
+        :isosceles
+      else
+        :scalene
+      end
+    end
+    class TriangleError < StandardError
+    end
+
+    
+
+    end
